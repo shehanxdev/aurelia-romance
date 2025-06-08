c48f213ffd70d935ea18f15a626497974f9635db
Shehan Chanuka <shehanxdev@gmail.com>
2025-06-08 19:57:35 +0530

    fix: fixed analytic import issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import App from "./App.tsx";
